e6aa9ebfff2d99a53c7cab43f8646d76be351d1e
German Ivanov <germanivanov0719@gmail.com>
2022-05-28 23:17:52 +0300

    Add design

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("home.ui", self)
+        # Your code
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
